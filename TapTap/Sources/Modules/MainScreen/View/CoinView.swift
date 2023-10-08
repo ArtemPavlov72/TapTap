2ee8d921e4a782f54c7efbcf976fa4ebd605aab0
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-10-08 20:54:10 +0500

    CoinView updated

--- TapTap/Sources/Modules/MainScreen/View/CoinView.swift
+++ TapTap/Sources/Modules/MainScreen/View/CoinView.swift
@@ -12,7 +12,10 @@ class CoinView: UIView {
 
   // MARK: - Private properties
 
+  /// добавляем объект вьюшки для отображения сцены
   private var sceneView = SCNView()
+
+  ///добавляем сцену
   private var scene = SCNScene()
 
   // MARK: - Initialization
@@ -42,20 +45,58 @@ private extension CoinView {
     let camera = SCNCamera()
     let cameraNode = SCNNode()
     cameraNode.camera = camera
-    cameraNode.position = SCNVector3(x: 0.0, y: 0.0, z: 3.0)
+    ///устанавливаем положение основного объекта
+    cameraNode.position = SCNVector3(x: -3.0, y: 3.0, z: 4.0)
 
+    /// добавляем дополнительные рассеянный свет
+    let ambientLight = SCNLight()
+    ambientLight.type = SCNLight.LightType.ambient
+    ambientLight.color = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+    cameraNode.light = ambientLight
+
+    /// основной свет точечный
     let light = SCNLight()
-    light.type = SCNLight.LightType.omni
+    light.type = SCNLight.LightType.spot
+    light.spotInnerAngle = 50.0
+    light.spotOuterAngle = 80.0
+    light.castsShadow = true
     let lightNode = SCNNode()
     lightNode.light = light
-    lightNode.position = SCNVector3(x: 1.5, y: 1.5, z: 1.5)
+    lightNode.position = SCNVector3(x: 1.5, y: 3.5, z: 3.5)
+
+    /// cube
 
     let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
     let cubeNode = SCNNode(geometry: cubeGeometry)
 
+    ///красим куб в красный
+    let redMaterial = SCNMaterial()
+    redMaterial.diffuse.contents = UIColor.red
+    cubeGeometry.materials = [redMaterial]
+
+    /// plane
+
+    let planeGeometry = SCNPlane(width: 50.0, height: 50.0)
+    let planeNode = SCNNode(geometry: planeGeometry)
+    planeNode.eulerAngles = SCNVector3(x: GLKMathDegreesToRadians(-90), y: 0, z: 0)
+    planeNode.position = SCNVector3(x: 0, y: -0.5, z: 0)
+
+    ///красим дополнительный объект в зеленый
+    let greenMaterial = SCNMaterial()
+    greenMaterial.diffuse.contents = UIColor.green
+    planeGeometry.materials = [greenMaterial]
+
+    /// добавляем свет, камеру и объекты на сцену
+
     scene.rootNode.addChildNode(lightNode)
     scene.rootNode.addChildNode(cameraNode)
     scene.rootNode.addChildNode(cubeNode)
+    scene.rootNode.addChildNode(planeNode)
+
+    let constraint = SCNLookAtConstraint(target: cubeNode)
+    constraint.isGimbalLockEnabled = true
+    cameraNode.constraints = [constraint]
+    lightNode.constraints = [constraint]
   }
 
   func configureLayout() {
